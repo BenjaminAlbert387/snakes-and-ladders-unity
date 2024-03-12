ff53279884e5c34e116b74b6a73aa378cc4c9c6d
Benjamin <158299275+BenjaminAlbert387@users.noreply.github.com>
2024-03-12 12:09:17 +0000

    continued to work on the dice script

--- SnakesAndLaddersExtra/Assets/Scripts/DiceScript.cs
+++ SnakesAndLaddersExtra/Assets/Scripts/DiceScript.cs
@@ -1,21 +1,46 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor.Rendering;
 using UnityEngine;
 
+//Reference: 
 public class DiceScript : MonoBehaviour
 {
-    // Initialise an integer, as we will be using its value for player movement on the game board//
+    // Initialise an integer, as we will be using its value for player movement on the game board
     int DiceRollValue;
 
-    // SerializeField is used so Unity can view and edit non public assets//
+    // SerializeField is used, so Unity can view and edit non public assets
     [SerializeField]
 
-    // List the six dice sprites 
+    // List the six dice sprites. 
     List<Sprite> die;
 
-    // Create a function that simulates the dice "rolling" in 2D.
+    // Create a function that simulates the dice "rolling" in 2D
     public void DiceRollImage()
     {
         // Use Unity's SpriteRender built in function to go through each dice face
+        SpriteRenderer renderer = GetComponent<SpriteRenderer>();
+
+        // The dice selectes a dice face randomly, between 1 to 6
+        renderer.sprite = die[Random.Range(0, die.Count)];
+    }
+    
+    public void DiceRollSetImage()
+    {
+        // Sets up the dice face selected that will be outputted to the player
+        SpriteRenderer renderer = GetComponent<SpriteRenderer>();
+
+        // As the dice only goes up to 6, take away one
+        renderer.sprite = die[DiceRollValue - 1];
+    }
+
+    public void DiceRoll(int TemporaryValue)
+    {
+        // DiceRollValue becomes equal to the temporary value
+        DiceRollValue = TemporaryValue;
+
+        // Plays the dice roll animation created in Unity
+        Animator animator = GetComponent<Animator>();
+        animator.Play("Roll dice", -1, 0f);
     }
 }
--- SnakesAndLaddersExtra/Assets/Scripts/GameManager.cs
+++ SnakesAndLaddersExtra/Assets/Scripts/GameManager.cs
@@ -5,12 +5,20 @@ using System.Runtime.CompilerServices;
 using UnityEngine;
 
 //Reference: [insert youtube link]
+
 public class GameManager : MonoBehaviour
+
 {
+    //Boolean that checks whether the dice is able to be pressed or not
+    public bool hasGameFinished, canClick;
+
+    // Creates an instance of GameManager. Instance is a "copy" of something
+    public static GameManager instance;
+
     //Loads the game when the application is opened 
     public void RestartGame()
     {
-        // Loads the first scene 
+        // Loads the first scene when the game is restarted.
         UnityEngine.SceneManagement.SceneManager.LoadScene(0);
     }
 
@@ -24,5 +32,26 @@ public class GameManager : MonoBehaviour
         // Close the application
             Application.Quit();
         // Else continue playing
+    }
+
+    // Initalises the dice when the game starts
+    private void Awake()
+    {
+        // If the game manager is equal to nothing
+        if (instance == null)
+        {
+            // Then initalise it
+            instance = this;
+        }
+        else
+        {
+            // Else remove it. If the game is over, then we don't need it to be initialsed. 
+            Destroy(gameObject);
+        }
+        
+        // Allows the dice to be clicked when the game is running.
+        canClick = true;
+        // This is only true when the game is not finished.
+        hasGameFinished = false;
     }
 }
